ac3faf8920928fabdb9c1e7bdc0ad06738d95c0e
damodar3103 <dneelapu@outlook.com>
2024-08-04 19:29:45 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import json
 from azure.identity import ClientSecretCredential
 from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.resourcegraph import ResourceGraphClient
+from azure.mgmt.resourcegraph.models import QueryRequest
 
 # Hard-coded Azure credentials and resource group details
 AZURE_CLIENT_ID = "<your_client_id>"
@@ -24,4 +26,14 @@ compute_client = ComputeManagementClient(credential, SUBSCRIPTION_ID)
 network_client = NetworkManagementClient(credential, SUBSCRIPTION_ID)
 resource_graph_client = ResourceGraphClient(credential)
 
-print("Clients initialized successfully")
+# Resource Graph query to get all VMs with their attributes
+query = QueryRequest(
+    subscriptions=[SUBSCRIPTION_ID],
+    query="resources | where type == 'microsoft.compute/virtualmachines'"
+)
+
+# Execute the query
+result = resource_graph_client.resources(query)
+
+# Print the query result for inspection
+print(json.dumps(result.data, indent=2))
