75464762cfb1c7b943caa62794ec0e8c8e7ae5de
damodar3103 <dneelapu@outlook.com>
2024-08-04 19:10:44 -0400

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import json
+from azure.identity import ClientSecretCredential
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.resourcegraph import ResourceGraphClient
+from azure.mgmt.resourcegraph.models import QueryRequest
+
+# Hard-coded Azure credentials and resource group details
+AZURE_CLIENT_ID = "<your_client_id>"
+AZURE_CLIENT_SECRET = "<your_client_secret>"
+AZURE_TENANT_ID = "<your_tenant_id>"
+SUBSCRIPTION_ID = "<your_subscription_id>"
+RESOURCE_GROUP = "<your_resource_group>"
+
+# Authenticate using ClientSecretCredential
+credential = ClientSecretCredential(
+    client_id=AZURE_CLIENT_ID,
+    client_secret=AZURE_CLIENT_SECRET,
+    tenant_id=AZURE_TENANT_ID
+)
+
+# Initialize the Compute and Resource Graph Clients
+compute_client = ComputeManagementClient(credential, SUBSCRIPTION_ID)
+network_client = NetworkManagementClient(credential, SUBSCRIPTION_ID)
+resource_graph_client = ResourceGraphClient(credential)
+
+# Resource Graph query to get all VMs with their tags
+query = QueryRequest(
+    subscriptions=[SUBSCRIPTION_ID],
+    query="resources | where type == 'microsoft.compute/virtualmachines' | project id, name, tags"
+)
+
+# Execute the query
+result = resource_graph_client.resources(query)
+
+# Initialize inventory structure
+inventory = {
+    "_meta": {
+        "hostvars": {}
+    },
+    "all": {
+        "hosts": [],
+        "children": []
+    }
+}
+
+# Process VMs and create inventory
+for vm in result.data:
+    vm_name = vm['name']
+    tags = vm.get('tags', {})
+
+    # Flatten tags with nested keys
+    flattened_tags = {}
+    for key, value in tags.items():
+        if isinstance(value, dict):
+            for sub_key, sub_value in value.items():
+                flattened_tags[f"{key}.{sub_key}"] = sub_value
+        else:
+            flattened_tags[key] = value
+
+    # Get VM IP address using Compute Management Client
+    vm_id = vm['id']
+    vm_obj = compute_client.virtual_machines.get(RESOURCE_GROUP, vm_name)
+    nic_id = vm_obj.network_profile.network_interfaces[0].id.split('/')[-1]
+    nic = network_client.network_interfaces.get(RESOURCE_GROUP, nic_id)
+    private_ip = nic.ip_configurations[0].private_ip_address
+
+    # Add VM to inventory
+    inventory["_meta"]["hostvars"][vm_name] = {
+        "ansible_host": private_ip,
+        **flattened_tags
+    }
+    inventory["all"]["hosts"].append(vm_name)
+
+# Print the inventory as JSON for testing
+print(json.dumps(inventory, indent=2))
